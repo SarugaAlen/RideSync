634d9da74b00882c4bc2c475ce994095de519e3b
alensaruga <alen.saruga01@gmail.com>
2025-05-14 18:13:05 +0200

    feat: add logging with pino and configure services for Loki, Promtail, and Grafana

--- services/user-service/server.ts
+++ services/user-service/server.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express, { Application } from 'express';
 import cors from 'cors';
+import pino from 'pino';
+import pinoHttp from 'pino-http';
 import { syncDatabase } from './src/infrastructure/database/database';
 import userRoutes from './src/presentation/routes/userRoutes';
 import swaggerRoutes from './src/presentation/routes/swagger';
@@ -10,6 +12,24 @@ dotenv.config();
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
+const logger = pino({
+    level: 'info',
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+            translateTime: 'SYS:standard',
+            destination: 1,
+        },
+    },
+});
+
+
+const pinoLogger = pinoHttp({
+    logger,
+});
+
+app.use(pinoLogger);
 app.use(express.json());
 app.use(cors());
 app.use('/users', userRoutes);
